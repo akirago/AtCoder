d0cfd5702ae59da89177f22d51bb3105b0099e4e
akirago <akirasyoku1234@gmail.com>
2020-08-29 22:40:00 +0900

    resolve abc 174 and 177

---
+++ abc/abc177/b.py
@@ -0,0 +1,83 @@
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+from math import floor, sqrt, factorial, hypot, log  # log2ないｙｐ
+from heapq import heappop, heappush, heappushpop
+from collections import Counter, defaultdict, deque
+from itertools import accumulate, permutations, combinations, product, combinations_with_replacement
+from bisect import bisect_left, bisect_right
+from copy import deepcopy
+from fractions import gcd
+from random import randint
+import difflib
+
+
+def ceil(a, b): return (a + b - 1) // b
+
+
+inf = float('inf')
+mod = 10 ** 9 + 7
+
+
+def pprint(*A):
+    for a in A:     print(*a, sep='\n')
+
+
+def INT_(n): return int(n) - 1
+
+
+def MI(): return map(int, input().split())
+
+
+def MF(): return map(float, input().split())
+
+
+def MI_(): return map(INT_, input().split())
+
+
+def LI(): return list(MI())
+
+
+def LI_(): return [int(x) - 1 for x in input().split()]
+
+
+def LF(): return list(MF())
+
+
+def LIN(n: int): return [I() for _ in range(n)]
+
+
+def LLIN(n: int): return [LI() for _ in range(n)]
+
+
+def LLIN_(n: int): return [LI_() for _ in range(n)]
+
+
+def LLI(): return [list(map(int, l.split())) for l in input()]
+
+
+def I(): return int(input())
+
+
+def F(): return float(input())
+
+
+def ST(): return input().replace('\n', '')
+
+
+def main():
+    S = ST()
+    T = ST()
+    m = 0
+    for i in range(len(S)-len(T)+1):
+        mn = 0
+        for j in range(len(T)):
+            if S[i+j] == T[j]:
+                mn += 1
+        m = max(m, mn)
+    print(len(T) - m)
+
+
+if __name__ == '__main__':
+    main()
---
+++ abc/abc177/e.py
@@ -0,0 +1,85 @@
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+from math import floor, sqrt, factorial, hypot, log, gcd  # log2ないｙｐ
+from heapq import heappop, heappush, heappushpop
+from collections import Counter, defaultdict, deque
+from itertools import accumulate, permutations, combinations, product, combinations_with_replacement
+from bisect import bisect_left, bisect_right
+from copy import deepcopy
+from fractions import gcd
+from random import randint
+
+
+def ceil(a, b): return (a + b - 1) // b
+
+
+inf = float('inf')
+mod = 10 ** 9 + 7
+
+
+def pprint(*A):
+    for a in A:     print(*a, sep='\n')
+
+
+def INT_(n): return int(n) - 1
+
+
+def MI(): return map(int, input().split())
+
+
+def MF(): return map(float, input().split())
+
+
+def MI_(): return map(INT_, input().split())
+
+
+def LI(): return list(MI())
+
+
+def LI_(): return [int(x) - 1 for x in input().split()]
+
+
+def LF(): return list(MF())
+
+
+def LIN(n: int): return [I() for _ in range(n)]
+
+
+def LLIN(n: int): return [LI() for _ in range(n)]
+
+
+def LLIN_(n: int): return [LI_() for _ in range(n)]
+
+
+def LLI(): return [list(map(int, l.split())) for l in input()]
+
+
+def I(): return int(input())
+
+
+def F(): return float(input())
+
+
+def ST(): return input().replace('\n', '')
+
+
+def main():
+    N = I()
+    A = LI()
+    ans = "pairwise coprime"
+    for i in range(2, 1001):
+        t = 0
+        for a in A:
+            if a % i == 0:
+                t += 1
+        if t == N:
+            ans = "not coprime"
+        elif t > 1:
+            ans = "setwise coprime"
+    print(ans)
+
+
+if __name__ == '__main__':
+    main()
